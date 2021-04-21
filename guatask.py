dd32b1b5f5f01966806475023b5d8f6211f11c5c
Miguel Garcia-Ortegon <mg770@cam.ac.uk>
2021-04-21 01:04:52 +0100

    Translate all the path operations from os.path to pathlib.Path

--- guatask.py
+++ guatask.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-import os
+from pathlib import Path
 from datetime import datetime
 import abc
 import torch
@@ -51,7 +51,7 @@ class Task(abc.ABC):
 
 
     # UTILITY PROPERTIES AND FUNCTIONS THAT ARE NOT TO BE SET MANUALLY # TODO Check the naming convention and whether it would be good to prepend an underscore or something
-    log_file_handler = None  # This will be set by the task manager later, and it's there because it can be used 
+    log_file_handler = None  # This will be set by the task manager later, and it's there because it can be used
                              # by subprocess to save the log of external executables, if needed
     @property
     def _path_to_tasks(self):
@@ -60,37 +60,45 @@ class Task(abc.ABC):
         or tasks.py) is located
         From  https://stackoverflow.com/questions/2092752/getting-the-module-file-path-of-a-derived-class-via-inheritance
         '''
-        return os.path.dirname(sys.modules[self.__class__.__module__].__file__)
+        # return os.path.dirname(sys.modules[self.__class__.__module__].__file__)
+        return Path(sys.modules[self.__class__.__module__].__file__).parent
     @property
     def input_filepath(self):
         """ Takes the directory and input filename and combines them together"""
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'INPUT',self.input_filename))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'INPUT',self.input_filename))
+        return (self._path_to_tasks / self.directory / 'INPUT' / self.input_filename).resolve()
     @property
     def log_filepath(self):
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory, 'LOG', 'task.log'))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory, 'LOG', 'task.log'))
+        return (self._path_to_tasks / self.directory / 'LOG' / 'task.log').resolve()
     @property
     def tmp_log_filepath(self):
         # The task being run is passed as an instance object (rather than as a class object), so to get the class name we need task.__class__.__name__
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory, 'LOG', self.__class__.__name__ + '.log'))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory, 'LOG', self.__class__.__name__ + '.log'))
+        return (self._path_to_tasks / self.directory / 'LOG' / (self.__class__.__name__ + '.log')).resolve()
     @property
     def output_filepath(self):
         """ Takes the directory, subdirectory and output filename and combines them together"""
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'OUTPUT',self.subdirectory,self.output_filename))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'OUTPUT',self.subdirectory,self.output_filename))
+        return (self._path_to_tasks / self.directory / 'OUTPUT' / self.subdirectory / self.output_filename).resolve()
     @property
     def input_dir(self):
         """ Returns the full path to the input directory"""
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'INPUT'))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'INPUT'))
+        return (self._path_to_tasks / self.directory / 'INPUT').resolve()
     @property
     def log_dir(self):
         """ Returns the full path to the log directory"""
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'LOG'))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'LOG'))
+        return (self._path_to_tasks / self.directory / 'LOG').resolve()
     @property
     def output_dir(self):
         """ Returns the full path to the output directory"""
-        return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'OUTPUT',self.subdirectory))
+        # return os.path.abspath(os.path.join(self._path_to_tasks,self.directory,'OUTPUT',self.subdirectory))
+        return (self._path_to_tasks / self.directory / 'OUTPUT' / self.subdirectory).resolve()
     @property
     def is_completed(self):
-        is_completed = os.path.exists(self.output_filepath)
+        is_completed = self.output_filepath.exists()
         return is_completed
     @property
     def are_dependencies_completed(self):
@@ -101,7 +109,7 @@ class Task(abc.ABC):
         else:
             for each_required_task in self.requires:
                 each_instance = each_required_task()
-                is_completed = os.path.exists(each_instance.output_filepath)
+                is_completed = each_instance.is_completed
                 is_completed_message = 'COMPLETE' if is_completed else 'INCOMPLETE'
                 # each_instance is a class instance, so to obtain the class name we do each_instance.__class__.__name__
                 print('\t' + each_instance.__class__.__name__, is_completed_message)
@@ -113,16 +121,15 @@ class Task(abc.ABC):
     # USUAL INIT
     def __init__(self):
         # Create output directory
-        # After creating it, the output directory can always be accessed through the @property method task.output_dir  
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
+        # After creating it, the output directory can always be accessed through the @property method task.output_dir
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(parents=True)
 
         # Create log directory
-        # After the log directory is created, we can get the paths for the log file and the tmp log file with the @property methods 
-        # task.log_filepath and task.tmp_log_filepath. 
-        log_directory = os.path.join(self.directory, 'LOG')
-        if not os.path.exists(log_directory):
-            os.makedirs(log_directory)
+        # After the log directory is created, we can get the paths for the log file and the tmp log file with the @property methods
+        # task.log_filepath and task.tmp_log_filepath.
+        if not self.log_dir.exists():
+            self.log_dir.mkdir(parents=True)
 
 
 
@@ -137,7 +144,7 @@ class PytorchTrainTask(TrainTask):  # TODO Implement all the features described 
         super().__init__()
         use_cuda = self.params['train_loop']['use_cuda']
         self.device = torch.device('cuda' if use_cuda else 'cpu')
-    def evaluate(self,dataloader,verbose=False): 
+    def evaluate(self,dataloader,verbose=False):
         '''Evaluate the performance of the model in its current state (which could be at any stage before, during or after training)
         - dataloader: Pytorch dataloader containing the dataset to evaluate on.'''
         metric = self.params['train_loop']['metric']
@@ -151,7 +158,7 @@ class PytorchTrainTask(TrainTask):  # TODO Implement all the features described 
             X = X.to(self.device)  # if the evaluation is to be executed outside of task.run, self.device should be set as a task attribute in PytorchTrainTask
             Y = Y.reshape(-1,1)
             Y = Y.double().to(self.device)
-            P = model(X) 
+            P = model(X)
             Y_all[idx*batch_size:(idx+1)*batch_size] = Y.detach().cpu().numpy()
             P_all[idx*batch_size:(idx+1)*batch_size] = P.detach().cpu().numpy()
             log_freq = 10
@@ -167,7 +174,7 @@ class PytorchTrainTask(TrainTask):  # TODO Implement all the features described 
         # Create path for checkpoint
         model_name = self.output_filename.split('.pt')[0]
         checkpoint_name = model_name + '_epoch' + str(n_epochs_so_far)
-        checkpoint_path = os.path.join(self.output_dir,checkpoint_name + '.pt')
+        checkpoint_path = self.output_dir / checkpoint_name + '.pt'
         # Save checkpoint
         torch.save(self.model.state_dict(), checkpoint_path)
 
@@ -177,7 +184,7 @@ class PytorchTrainTask(TrainTask):  # TODO Implement all the features described 
 
 def run_task(task_class):
     # If not all the abstract methods are defined, this will raise an error
-    task = task_class() 
+    task = task_class()
     # Redirect all output to tmp log file
     tmp_f = open(task.tmp_log_filepath, 'a')
     original_stdout = sys.stdout
@@ -208,7 +215,7 @@ def run_task(task_class):
         print('### ABORTING TASK ###')
     else:
         print('This task parameters are ', task.params)
-        # Run the task 
+        # Run the task
         task.run()
         # Print finishing time
         print('Finished at time: ', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
